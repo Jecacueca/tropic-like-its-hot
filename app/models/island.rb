3c1ae276c223c21f133fdb7925ec5b97734dbad1
meehir patel <meehir.patel12@gmail.com>
2020-11-16 16:32:07 +0000

    validations complete

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,3 +1,8 @@
 class Island < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :description, length: { minimum: 150, to_short: "#{count} characters is the minimum length." }
 end
