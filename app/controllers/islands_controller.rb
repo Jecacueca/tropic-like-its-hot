f23fc70d6c456627d553ac1684004b925f0835b2
Chiara Agazzi <gyggeaka@hotmail.com>
2020-11-17 12:19:00 +0100

    chiara1

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -1,2 +1,12 @@
 class IslandsController < ApplicationController
+  def show
+    @island = Island.find(params[:id])
+  end
+
+  private
+
+  def island_params
+    params.require(:island).permit(:name, :address, :description)
+  end
+
 end
--- app/models/island.rb
+++ app/models/island.rb
@@ -2,6 +2,7 @@ class Island < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
+  validates :name, presence: true
   validates :address, presence: true
   validates :description, presence: true, length: { minimum: 150 }
 end
