0ab4254616fdf2f209942de251dcfaf6aaf29474
Chiara Agazzi <gyggeaka@hotmail.com>
2020-11-17 17:10:58 +0100

    sperem

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,9 +5,19 @@ class BookingsController < ApplicationController
     redirect_to island_path(@booking.island)
   end
 
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking.island)
+  end
+
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :status)
+    params.require(:booking).permit(:start_date, :end_date, :number_of_people)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :island
 
+  validates :number_of_people, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :status, inclusion: { in: ["declined", "pending", "accepted"] }
