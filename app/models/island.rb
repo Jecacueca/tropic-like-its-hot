0091d7848c65bcdba6075ea3e678b91e237aaafb
meehir patel <meehir.patel12@gmail.com>
2020-11-19 15:25:43 +0000

    index page

--- app/models/island.rb
+++ app/models/island.rb
@@ -7,4 +7,7 @@ class Island < ApplicationRecord
   validates :price, presence: true
   validates :address, presence: true
   validates :description, presence: true, length: { minimum: 150 }
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
